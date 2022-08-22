f1843c73aa96795a5c63b2ad0f1764b06c56fbe8
Thilagarajan Kamarajan <kthilagarajan@gmail.com>
2022-08-22 16:40:17 +0530

    Build Changes

--- src/commons/environment.ts
+++ src/commons/environment.ts
@@ -1,4 +1,4 @@
-export const CURRENT_ENV: "DEV" | "PROD" = "DEV";
+export const CURRENT_ENV: "DEV" | "PROD" = "PROD";
 
 const VARIABLES = {
   DEV: {
@@ -10,7 +10,7 @@ const VARIABLES = {
   PROD: {
     WEB_CLIENT_ID:
       "383446887463-m54vlaqlcu2r95obouiinj0r3j42m0n2.apps.googleusercontent.com",
-    EMAIL_VERIFICATION_URL: "https://homeservices-b769d.web.app",
+    EMAIL_VERIFICATION_URL: "https://homeservices-b769d.firebaseapp.com",
     API_BASE_URL: "https://miohomeservices.com/api/v2",
   },
 };
